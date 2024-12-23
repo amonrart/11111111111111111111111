89629cde73021bf1690efc0116e3ce34638d6333
amonrart <amonrart.m@kkumail.com>
2024-12-23 15:49:40 +0700

    1111111111111111111111111111111111111

--- lib/php_project/db_connect.php
+++ lib/php_project/db_connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; // ชื่อเซิร์ฟเวอร์
 $username = "root";       // ชื่อผู้ใช้ (ค่าเริ่มต้นคือ root)
 $password = "";           // รหัสผ่าน (ค่าเริ่มต้นคือว่างเปล่า)
-$dbname = "ตามรอยรีวิว"; // ชื่อฐานข้อมูล
+$dbname = "จำนวนหมวดหมู่"; // ชื่อฐานข้อมูล
 
 // สร้างการเชื่อมต่อ
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,4 +13,3 @@ if ($conn->connect_error) {
 }
 echo "เชื่อมต่อฐานข้อมูลสำเร็จ!";
 ?>
-C:\Users\AdviceIT\Downloads\php-8.4.2\php-8.4.2
